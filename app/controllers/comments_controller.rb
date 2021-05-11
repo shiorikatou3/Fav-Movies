6f5586e057dc7a1ff0d55fa9d84e0700c8d0eb81
shiori.kato <shakespeare14.amika@gmail.com>
2021-05-11 06:03:41 +0000

    seed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   private
   
   def comment_params
-    params.require(:comment).permit(:content)
+    params.permit(:content)
   end
   
   def correct_user
